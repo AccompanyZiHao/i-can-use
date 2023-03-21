05d52f568c0f14276fc69b2911628686967640c0
wuxiaobai <1548928591@qq.com>
2023-03-21 18:37:57 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts" nam="i-can-use"></script>
+
+<template>
+  <router-view v-slot="{ Component, route }">
+    <keep-alive>
+      <component
+        :is="Component"
+        :key="route.name"
+        v-if="route.meta.keepAlive"
+      />
+    </keep-alive>
+    <component :is="Component" :key="route.name" v-if="!route.meta.keepAlive" />
+  </router-view>
+</template>
+
+<style scoped></style>

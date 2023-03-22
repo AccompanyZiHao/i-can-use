e9c69736ee4cb0d5e89ba427ea9919c99f09331d
wuxiaobai <1548928591@qq.com>
2023-03-22 10:16:59 +0800

    Update arrSplit.ts

--- src/utils/arrSplit.ts
+++ src/utils/arrSplit.ts
@@ -2,7 +2,7 @@
  * @description: 数组按 size 大小分组，每一组的长度为 size
  * @param {*} arr: 需要处理的数组
  * @param {*} size: 数组大小
- * @return {*} 返回一个新的数组
+ * @return {*} 返回一个数组
  */
 export const arrSplitChunk = <T>(arr: T[], size: number): T[] => {
   let result = [];
@@ -16,7 +16,7 @@ export const arrSplitChunk = <T>(arr: T[], size: number): T[] => {
  * @description: 把数组分为 size 组，二维数组的长度为 size
  * @param {*} arr: 需要处理的数组
  * @param {*} size: 二维数组的长度
- * @return {*} 返回一个新的二维数组
+ * @return {*} 返回一个二维数组
  */
 export const arrSplitChunk2 = <T>(arr: T[], size: number): T[][] => {
   let result = Array.from({ length: size }, () => []);

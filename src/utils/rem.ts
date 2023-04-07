dfcf24b7b2a59de3a814023f711a461bd5aa7eee
wuxiaobai <1548928591@qq.com>
2023-04-07 18:25:17 +0800

    Update rem.ts

--- src/utils/rem.ts
+++ src/utils/rem.ts
@@ -23,3 +23,51 @@ export function setRem() {
 window.onresize = function () {
   setRem();
 };
+
+// 获取 当前页面 document 的 font-size
+export const getFontSize: number | null = (() => {
+  let clientWidth = document.documentElement.clientWidth || null;
+  if (!clientWidth) return null;
+  clientWidth = clientWidth > 750 ? 750 : clientWidth;
+  return 100 * (clientWidth / 750);
+})();
+
+let isM = JSON.parse(sessionStorage.getItem('isM')) || false;
+// 判断是否是移动端
+export function isMobile() {
+  const mobileAgents = [
+    'iphone',
+    'ipod',
+    'ipad',
+    'android',
+    'mobile',
+    'blackberry',
+    'webos',
+    'incognito',
+    'webmate',
+    'bada',
+    'nokia',
+    'lg',
+    'ucweb',
+    'wap',
+    'skyfire',
+    'mobile',
+    'symbian',
+    'windows ce',
+  ];
+  const Browser = navigator.userAgent.toLowerCase();
+  let isMobile = false;
+  for (var i = 0; i < mobileAgents.length; i++) {
+    if (Browser.indexOf(mobileAgents[i]) != -1) {
+      isMobile = true;
+      break;
+    }
+  }
+  if (isM != isMobile) {
+    // 移动端和PC端切换时，刷新页面
+    location.reload();
+  }
+  isM = isMobile;
+  sessionStorage.setItem('isM', JSON.stringify(isM));
+  return isMobile;
+}
